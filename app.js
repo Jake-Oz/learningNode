aad16ea6acac225575a68eea43f2339ec02b887c
Jake-Oz <jakeandrach@me.com>
2022-07-02 15:51:05 +1000

    Working with async features

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  console.log("request Event");
+  res.end("Hello World from Server");
+});
+
+server.listen(5000, () => {
+  console.log("Listening on Port : 5000...");
+});
